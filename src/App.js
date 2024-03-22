561f9b47dce257480c85f3985d6d2b124aa28a31
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2024-03-22 14:48:06 +0100

    Fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Card from "./container/Card";
 
 function App() {
-  return <div className="App" role="main"></div>;
+  return (
+    <div className="App" role="main">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
--- src/container/Card.jsx
+++ src/container/Card.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./card.css";
+import CardImage from "../assets/images/illustration-article.svg";
+import Avatar from "../assets/images/image-avatar.webp";
 
 function Card() {
-  return <div></div>;
+  return (
+    <div className="card__container">
+      <div className="card__image">
+        <img src={CardImage} alt="CardImage" />
+      </div>
+      <div className="card__info">
+        <div className="card__info_type">
+          <p>Learning</p>
+        </div>
+        <div className="card__info_date">
+          <p>Published 21 Dec 2023</p>
+        </div>
+        <div className="card__info_code">
+          <p>HTML & CSS foundations</p>
+        </div>
+        <div className="card__info_text">
+          <p>
+            These languages are the backbone of every website, defining
+            structure, content, and presentation.
+          </p>
+        </div>
+        <div className="card__info_avatar">
+          <img src={Avatar} alt="avatar" />
+          <p>Greg Hooper</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
